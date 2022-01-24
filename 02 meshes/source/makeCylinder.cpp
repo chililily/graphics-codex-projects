8d048fe45b39f2064358b6c01ed476c7e96417bf
chililily <qili@reed.edu>
2022-01-23 23:11:48 -0800

    Added function that generates cylinder model and adds it to scene

--- 02 meshes/source/makeCylinder.cpp
+++ 02 meshes/source/makeCylinder.cpp
@@ -1,3 +1,5 @@
+// Create a procedure (function or method) named makeCylinder that generates a file data-files/model/cylinder.off given a radius and height. This file should be an indexed convex-polygon mesh for a cylinder about the y-axis that is centered at the origin. 
+
 #include "makeCylinder.h"
 
 void makeCylinder(double radius, double height) {
